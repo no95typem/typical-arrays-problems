86b797725e4b9378997a840c8eda81bda7b11315
no95typem <50191552+no95typem@users.noreply.github.com>
2021-02-16 20:16:41 +0400

    feat: add min, max and avg functions logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(...args) {
+  let min;
+  for (let arg of args) {
+    if (typeof arg == "number") {
+      if (typeof min == "undefined") min = arg;
+      else if (min > arg) min = arg;
+    }
+  }
+  if (typeof min == "undefined") min = 0;
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(...args) {
+  let max;
+  for (let arg of args) {
+    if (typeof arg == "number") {
+      if (typeof max == "undefined") max = arg;
+      else if (max < arg) max = arg;
+    }
+  }
+  if (typeof max == "undefined") max = 0;
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(...args) {
+  let avg = 0;
+  let counter = 0;
+  for (let arg of args) {
+    if (typeof arg == "number") {
+      avg += arg;
+      counter++;
+    }
+  }
+  if (counter > 0) avg = avg / counter;
+  return avg;
 }
